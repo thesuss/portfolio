2251356634b1094c8d2583967f8cdff455c3c21b
thesuss <lullisuss@gmail.com>
2016-08-29 14:21:52 +0200

    insterted a footer partial

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -11,4 +11,12 @@ describe 'index', type: :feature do
     expect(page).to have_content 'Middleman'
   end
 
+  it 'renders footer partial' do
+    expect(page).to have_selector 'footer'
+    within 'footer' do
+      expect(page).to have_content 'My Portfolio'
+      expect(page).to have_content 'Built using the awesome Middleman framework'
+    end
+  end
+
 end
