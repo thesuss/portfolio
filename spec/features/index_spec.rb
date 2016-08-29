25ae38a21f112c9b50e0af310a3ec619cac6112f
thesuss <lullisuss@gmail.com>
2016-08-29 14:51:39 +0200

    Reverted to "insterted a footer partial"
    This reverts commit 2251356634b1094c8d2583967f8cdff455c3c21b.

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -11,12 +11,4 @@ describe 'index', type: :feature do
     expect(page).to have_content 'Middleman'
   end
 
-  it 'renders footer partial' do
-    expect(page).to have_selector 'footer'
-    within 'footer' do
-      expect(page).to have_content 'My Portfolio'
-      expect(page).to have_content 'Built using the awesome Middleman framework'
-    end
-  end
-
 end
