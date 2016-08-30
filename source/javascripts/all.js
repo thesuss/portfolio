496c64435b89f63190851e09132e038fc52994ab
thesuss <lullisuss@gmail.com>
2016-08-30 16:28:03 +0200

    all of the template JS is loading well

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,22 @@
 // This is where it all goes :)
+function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
+
+require('javascripts/bootstrap.js')
+require('javascripts/wow.min.js')
+require('javascripts/jquery.fancybox.js')
+require('javascripts/main.js')
+require('javascripts/npm.js')
+require('javascripts/owl.carousel.min.js')
+require('javascripts/slider.js')
